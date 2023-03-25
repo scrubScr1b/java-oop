422d954a9e789bff45f9005f9739b7410ac5978d
scrubScr1b <ciwifc24@gmail.com>
2023-03-25 09:24:54 +0700

    Ini pertemuan ke 4

---
+++ P4/java_project/src/Animal.java
@@ -0,0 +1,11 @@
+class Animal {
+    String family;
+
+    Animal(String family) {
+        this.family = family;
+    }
+
+    void makeSound(String sound) {
+        System.out.println(sound);
+    }
+}
